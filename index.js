2a3a064f322dfb5379166cc998733089229db5c0
bryanmmr <bryanmmr@gmail.com>
2023-11-13 23:28:32 -0500

    added http server

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+const http = require("http");
 const puppeteer = require("puppeteer"); 
- 
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(async (req, res) => {
+    //set the request route
+    if (req.url === "/character" && req.method === "GET") {
+        //response headers
+        const response = await pbsKidsCharacters()
+        console.log(response)
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(response));
+    }
+
+    // If no route present
+    else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Route not found" }));
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`server started on port: ${PORT}`);
+});
+
 async function pbsKidsCharacters() { 
 	try { 
 		// Specify the URL of the dev.to tags web page 
@@ -45,14 +69,10 @@ async function pbsKidsCharacters() {
             })
             return newResults;
         });
-        console.log(newCall)
-		await browser.close(); 
+
+		await browser.close();
+        return newCall;
 	} catch (error) { 
 		console.error(error); 
 	} 
 } 
- 
-pbsKidsCharacters();
-
-
-// const randomItem = Math.floor(Math.random() * results.length);
